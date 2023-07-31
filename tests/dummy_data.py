4510198c0d3801723be774535fd3808f207f9c25
tin.a.yee <tin.a.yee@frontiir.net>
2023-08-01 01:03:17 +0630

    added testing for card controller

---
+++ tests/dummy_data.py
@@ -0,0 +1,9 @@
+category_payload = {"name": "fiction", "description": "This is description"}
+
+card_payload = {
+    "name": "Test Card",
+    "status": "draft",
+    "category": "Test Category",
+    "author": "Test User",
+    "content": "Test Content",
+}
